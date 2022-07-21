f61169949d062ad500ea2222f595fc38610eb7e0
karan-push <karan@epns.io>
2022-07-21 14:17:07 +0530

    adding mobile components from frontend sdk

--- dist/components/view/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-declare const View: () => JSX.Element;
-export default View;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
-export { default as View } from './view';
+export { default as IPFSIcon } from './ipfsicon';
+export { default as Notification } from './notification';
+export { default as ParseText } from './parseText';
